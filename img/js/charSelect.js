d6635b5d60185e4ee9f6e3d340a580a36c1b99e0
6m10cmUK <106600273+6m10cmUK@users.noreply.github.com>
2023-05-17 18:31:28 +0900

    Add files via upload

---
+++ img/js/charSelect.js
@@ -0,0 +1,329 @@
+$(function() {
+
+    var name = ["ami", "ane", "chiwa", "cta", "fran", "gale", "han", "imo", "imo", "kyomu", "mike", "retoro", "sayu", "shigure", "takei", "via", "yoro", "yupi", "zen"];
+
+    $(`.${name[0]}_bt`).hover(
+      function(e) {
+        $("."+name[0]).addClass("highlight");
+      },
+      function(e) {
+        $("."+name[0]).removeClass("highlight");
+      }
+    );
+
+    $(`.${name[1]}_bt`).hover(
+        function(e) {
+          $("."+name[1]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[1]).removeClass("highlight");
+        }
+      );
+  
+      $(`.${name[2]}_bt`).hover(
+        function(e) {
+          $("."+name[2]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[2]).removeClass("highlight");
+        }
+      );
+      $(`.${name[3]}_bt`).hover(
+        function(e) {
+          $("."+name[3]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[3]).removeClass("highlight");
+        }
+      );
+      $(`.${name[4]}_bt`).hover(
+        function(e) {
+          $("."+name[4]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[4]).removeClass("highlight");
+        }
+      );
+      $(`.${name[5]}_bt`).hover(
+        function(e) {
+          $("."+name[5]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[5]).removeClass("highlight");
+        }
+      );
+      $(`.${name[6]}_bt`).hover(
+        function(e) {
+          $("."+name[6]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[6]).removeClass("highlight");
+        }
+      );
+      $(`.${name[7]}_bt`).hover(
+        function(e) {
+          $("."+name[7]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[7]).removeClass("highlight");
+        }
+      );
+      $(`.${name[8]}_bt`).hover(
+        function(e) {
+          $("."+name[8]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[8]).removeClass("highlight");
+        }
+      );
+      $(`.${name[9]}_bt`).hover(
+        function(e) {
+          $("."+name[9]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[9]).removeClass("highlight");
+        }
+      );
+      $(`.${name[10]}_bt`).hover(
+        function(e) {
+          $("."+name[10]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[10]).removeClass("highlight");
+        }
+      );
+      $(`.${name[11]}_bt`).hover(
+        function(e) {
+          $("."+name[11]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[11]).removeClass("highlight");
+        }
+      );
+      $(`.${name[12]}_bt`).hover(
+        function(e) {
+          $("."+name[12]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[12]).removeClass("highlight");
+        }
+      );
+      $(`.${name[13]}_bt`).hover(
+        function(e) {
+          $("."+name[13]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[13]).removeClass("highlight");
+        }
+      );
+      $(`.${name[14]}_bt`).hover(
+        function(e) {
+          $("."+name[14]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[14]).removeClass("highlight");
+        }
+      );
+      $(`.${name[15]}_bt`).hover(
+        function(e) {
+          $("."+name[15]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[15]).removeClass("highlight");
+        }
+      );
+      $(`.${name[16]}_bt`).hover(
+        function(e) {
+          $("."+name[16]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[16]).removeClass("highlight");
+        }
+      );
+      $(`.${name[17]}_bt`).hover(
+        function(e) {
+          $("."+name[17]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[17]).removeClass("highlight");
+        }
+      );
+      $(`.${name[18]}_bt`).hover(
+        function(e) {
+          $("."+name[18]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[18]).removeClass("highlight");
+        }
+      );  
+
+      $(`.${name[0]}_bt`).click(()=>{profileView(name[0]);});
+      $(`.${name[1]}_bt`).click(()=>{profileView(name[1]);});
+      $(`.${name[2]}_bt`).click(()=>{profileView(name[2]);});
+      $(`.${name[3]}_bt`).click(()=>{profileView(name[3]);});
+      $(`.${name[4]}_bt`).click(()=>{profileView(name[4]);});
+      $(`.${name[5]}_bt`).click(()=>{profileView(name[5]);});
+      $(`.${name[6]}_bt`).click(()=>{profileView(name[6]);});
+      $(`.${name[7]}_bt`).click(()=>{profileView(name[7]);});
+      $(`.${name[8]}_bt`).click(()=>{profileView(name[8]);});
+      $(`.${name[9]}_bt`).click(()=>{profileView(name[9]);});
+      $(`.${name[10]}_bt`).click(()=>{profileView(name[10]);});
+      $(`.${name[11]}_bt`).click(()=>{profileView(name[11]);});
+      $(`.${name[12]}_bt`).click(()=>{profileView(name[12]);});
+      $(`.${name[13]}_bt`).click(()=>{profileView(name[13]);});
+      $(`.${name[14]}_bt`).click(()=>{profileView(name[14]);});
+      $(`.${name[15]}_bt`).click(()=>{profileView(name[15]);});
+      $(`.${name[16]}_bt`).click(()=>{profileView(name[16]);});
+      $(`.${name[17]}_bt`).click(()=>{profileView(name[17]);});
+      $(`.${name[18]}_bt`).click(()=>{profileView(name[18]);});
+      $(`.${name[19]}_bt`).click(()=>{profileView(name[19]);});
+
+      $(".back").click(()=>{
+        console.log("hoge");
+        $(".profile").css({display:"none"})
+        $(".profile img").attr({src:``}).css({
+            opacity:0,
+            left: "0"
+        });
+        $(".profile .discription").css({
+            opacity:0,
+            left: "670px"
+        });
+        $(".profile .profile_circle").css({
+            width: "0",
+            height: "0"
+        });
+        $(".profile .discription .name").html();
+        $(".profile .discription .content").html();
+        $(".backdrop").css({
+            display:"none",
+            opacity:0
+        })
+
+      });
+    
+});
+
+function profileView(name){
+    $(".profile").css({display:"block"})
+    $(".profile img").attr({src:`img/member/${name}.png`});
+    $(".profile .discription .name").html(member[name].name);
+    $(".profile .discription .content").html(member[name].discription);
+    $(".backdrop").css({display:"block"})
+    .animate({
+        opacity: 1
+    },400);
+    $(".profile .profile_circle")
+    .css({"background":member[name].color})
+    .animate({
+        width: "500px",
+        height: "500px"
+    },500);
+    setTimeout(()=>{
+        $(".profile img").animate({
+            opacity:1,
+            left: "-120px"
+        },500);
+    },200);
+    setTimeout(()=>{
+        $(".discription").animate({
+            opacity:1,
+            left: "570px"
+        }, 500);
+    },300);
+
+}
+
+
+let member = {
+    "ami":{
+        color:"linear-gradient(90deg, #13bee9, #4bbe95)",
+        name:"あみ",
+        discription:"hogehogehoge"
+    },
+    "ane":{
+        color:"#b28194",
+        name:"姉ちゃん",
+        discription:"hogehogehoge"
+    },
+    "chiwa":{
+        color:"#cc4474",
+        name:"ちわ小太郎",
+        discription:"hogehogehoge"
+    }, 
+    "cta":{
+        color:"#29f6fb",
+        name:"C太",
+        discription:"hogehogehoge"
+    }, 
+    "fran":{
+        color:"#a9fc18",
+        name:"Frandle256",
+        discription:"hogehogehoge"
+    }, 
+    "gale":{
+        color:"#82c6b7",
+        name:"gale",
+        discription:"hogehogehoge"
+    }, 
+    "han":{
+        color:"linear-gradient(90deg,#a599a5 ,#d1ccb6)",
+        name:"3010",
+        discription:"hogehogehoge"
+    }, 
+    "imo":{
+        color:"#7a5f72",
+        name:"いもうと",
+        discription:"hogehogehoge"
+    }, 
+    "kyomu":{
+        color:"#890000",
+        name:"虚無虚無ぷりん",
+        discription:"hogehogehoge"
+    }, 
+    "mike":{
+        color:"#e58d1f",
+        name:"若草みけ",
+        discription:"hogehogehoge"
+    }, 
+    "retoro":{
+        color:"#555555",
+        name:"れとろ",
+        discription:"hogehogehoge"
+    }, 
+    "sayu":{
+        color:"#aaeddb",
+        name:"白湯丸太",
+        discription:"hogehogehoge"
+    }, 
+    "shigure":{
+        color:"#fb0304",
+        name:"ふみしぐれ",
+        discription:"hogehogehoge"
+    }, 
+    "takei":{
+        color:"#89ead7",
+        name:"夕ケイ",
+        discription:"hogehogehoge"
+    }, 
+    "via":{
+        color:"#728474",
+        name:"VIA",
+        discription:"hogehogehoge"
+    }, 
+    "yoro":{
+        color:"#3a685e",
+        name:"吉岡よろ",
+        discription:"hogehogehoge"
+    }, 
+    "yupi":{
+        color:"#0060ff",
+        name:"木林ユピテル",
+        discription:"hogehogehoge"
+    }, 
+    "zen":{
+        color:"#c11dff",
+        name:"漸化式",
+        discription:"hogehogehoge"
+    }
+}
---
+++ img/js/index.js
@@ -0,0 +1,78 @@
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	//spanタグを追加する
+	var element = $(".TextTyping");
+	element.each(function () {
+		var text = $(this).html();
+		var textbox = "";
+		text.split('').forEach(function (t) {
+			if (t !== " ") {
+				textbox += '<span>' + t + '</span>';
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+
+	});
+    setTimeout(()=>{
+        TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
+    },1000);
+
+    setTimeout(()=>{
+        $(".circle").css({display:"block"})
+        .animate({
+            width:"5000px",
+            height:"5000px"
+        }, 2000)
+
+        setTimeout(()=>{
+            $(".triangle").css({display:"block"})
+            .animate({
+                width: "3464px",
+                height: "3000px",
+            },2000);
+        },500);
+
+        setTimeout(()=>{
+            $(".triangle_img").css({display:"block"})
+            .animate({
+                width: "3464px",
+                height: "3000px",
+            },2000);
+        },800);
+        setTimeout(()=>{
+            $(".circle").css({display:"none"})
+            $(".triangle").css({display:"none"})
+            $(".characters").css({display:"block"});
+            $(".left_menu").animate({
+                opacity:"1",
+                left:"20px"
+            },500);
+        },2800);
+    },4000);
+
+
+});
+
+function TextTypingAnime() {
+	$('.TextTyping').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		var thisChild = "";
+		if (scroll >= elemPos - windowHeight) {
+			thisChild = $(this).children(); 
+			thisChild.each(function (i) {
+				var time = 200;
+				$(this).delay(time * i).fadeIn(time);
+			});
+		} else {
+			thisChild = $(this).children();
+			thisChild.each(function () {
+				$(this).stop();
+				$(this).css("display", "none");
+			});
+		}
+	});
+}
