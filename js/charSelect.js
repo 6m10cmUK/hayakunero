62e1944236d2393dc612c7b2b67b05b7c1851006
6m10cmUK <106600273+6m10cmUK@users.noreply.github.com>
2023-05-17 16:58:06 +0900

    Add files via upload

---
+++ js/charSelect.js
@@ -0,0 +1,159 @@
+$(function() {
+
+    var name = ["ami", "ane", "chiwa", "cta", "fran", "gale", "han", "imo", "imo", "kyomu", "mike", "retoro", "sayu", "shigure", "takei", "via", "yoro", "yupi", "zen"];
+
+    $(`.${name[0]}_bt`).hover(
+      function(e) {
+        console.log("nande");
+        $("."+name[0]).addClass("highlight");
+      },
+      function(e) {
+        $("."+name[0]).removeClass("highlight");
+      }
+    );
+
+    $(`.${name[1]}_bt`).hover(
+        function(e) {
+          $("."+name[1]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[1]).removeClass("highlight");
+        }
+      );
+  
+      $(`.${name[2]}_bt`).hover(
+        function(e) {
+          $("."+name[2]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[2]).removeClass("highlight");
+        }
+      );
+      $(`.${name[3]}_bt`).hover(
+        function(e) {
+          $("."+name[3]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[3]).removeClass("highlight");
+        }
+      );
+      $(`.${name[4]}_bt`).hover(
+        function(e) {
+          $("."+name[4]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[4]).removeClass("highlight");
+        }
+      );
+      $(`.${name[5]}_bt`).hover(
+        function(e) {
+          $("."+name[5]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[5]).removeClass("highlight");
+        }
+      );
+      $(`.${name[6]}_bt`).hover(
+        function(e) {
+          $("."+name[6]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[6]).removeClass("highlight");
+        }
+      );
+      $(`.${name[7]}_bt`).hover(
+        function(e) {
+          $("."+name[7]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[7]).removeClass("highlight");
+        }
+      );
+      $(`.${name[8]}_bt`).hover(
+        function(e) {
+          $("."+name[8]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[8]).removeClass("highlight");
+        }
+      );
+      $(`.${name[9]}_bt`).hover(
+        function(e) {
+          $("."+name[9]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[9]).removeClass("highlight");
+        }
+      );
+      $(`.${name[10]}_bt`).hover(
+        function(e) {
+          $("."+name[10]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[10]).removeClass("highlight");
+        }
+      );
+      $(`.${name[11]}_bt`).hover(
+        function(e) {
+          $("."+name[11]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[11]).removeClass("highlight");
+        }
+      );
+      $(`.${name[12]}_bt`).hover(
+        function(e) {
+          $("."+name[12]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[12]).removeClass("highlight");
+        }
+      );
+      $(`.${name[13]}_bt`).hover(
+        function(e) {
+          $("."+name[13]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[13]).removeClass("highlight");
+        }
+      );
+      $(`.${name[14]}_bt`).hover(
+        function(e) {
+          $("."+name[14]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[14]).removeClass("highlight");
+        }
+      );
+      $(`.${name[15]}_bt`).hover(
+        function(e) {
+          $("."+name[15]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[15]).removeClass("highlight");
+        }
+      );
+      $(`.${name[16]}_bt`).hover(
+        function(e) {
+          $("."+name[16]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[16]).removeClass("highlight");
+        }
+      );
+      $(`.${name[17]}_bt`).hover(
+        function(e) {
+          $("."+name[17]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[17]).removeClass("highlight");
+        }
+      );
+      $(`.${name[18]}_bt`).hover(
+        function(e) {
+          $("."+name[18]).addClass("highlight");
+        },
+        function(e) {
+          $("."+name[18]).removeClass("highlight");
+        }
+      );  });
--- js/index.js
+++ js/index.js
@@ -44,14 +44,17 @@ $(window).on('load', function () {
         setTimeout(()=>{
             $(".circle").css({display:"none"})
             $(".triangle").css({display:"none"})
+            $(".characters").css({display:"block"});
+            $(".left_menu").animate({
+                opacity:"1",
+                left:"20px"
+            },500);
         },2800);
     },4000);
 
 
-});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+});
 
-
-// TextTypingというクラス名がついている子要素（span）を表示から非表示にする定義
 function TextTypingAnime() {
 	$('.TextTyping').each(function () {
 		var elemPos = $(this).offset().top - 50;
@@ -59,18 +62,16 @@ function TextTypingAnime() {
 		var windowHeight = $(window).height();
 		var thisChild = "";
 		if (scroll >= elemPos - windowHeight) {
-			thisChild = $(this).children(); //spanタグを取得
-			//spanタグの要素の１つ１つ処理を追加
+			thisChild = $(this).children(); 
 			thisChild.each(function (i) {
 				var time = 200;
-				//時差で表示する為にdelayを指定しその時間後にfadeInで表示させる
 				$(this).delay(time * i).fadeIn(time);
 			});
 		} else {
 			thisChild = $(this).children();
 			thisChild.each(function () {
-				$(this).stop(); //delay処理を止める
-				$(this).css("display", "none"); //spanタグ非表示
+				$(this).stop();
+				$(this).css("display", "none");
 			});
 		}
 	});
